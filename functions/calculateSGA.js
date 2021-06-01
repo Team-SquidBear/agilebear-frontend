aef7266a2945aed98a07e7cb6865567234cdb78a
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2021-06-01 10:56:06 -0400

    Clean up function

--- functions/calculateSGA.js
+++ functions/calculateSGA.js
@@ -1,16 +1,16 @@
 /**
  * Calculates Summary Group Average
  * 
+ * @api {get} /api/calculateSGA Calculate SGA value
  * @param {ita} Number Summed Individual Trait Average 
  * @param {sailorCount} Number Count of Sailors in given group 
- * @returns An object containing the raw SGA and rounded SGA values 
+ * @apiSuccess (200) {Object} raw `sga` and `roundedSGA` object
  */ 
-exports.handler = async(event) => {
+exports.handler = async event => {
     const givenITA = event.queryStringParameters.ita;
     const givenSailorCount = event.queryStringParameters.sailorCount;
-
     const calculatedSGA = givenITA / givenSailorCount;
-    
+
     return {
         statusCode: 200,
         body: JSON.stringify({ 
